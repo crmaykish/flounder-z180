9cf654c344733be7f93e8d62cbb0222f3d194e22
Colin Maykish <crmaykish@gmail.com>
2022-08-08 21:14:40 -0400

    Removed bus spy tool

--- tools/bus_spy/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Open a Docker container and build the Pico project
-
-# Build the "picodev" image
-docker build -t picodev .
-
-# Launch a "picodev" container and run the build script
-docker run --user pico -v ~/Workspace:/home/pico/Workspace -it picodev bash /home/pico/Workspace/flounder-z180/tools/bus_spy/build_internal.sh
--- tools/bus_spy/flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo mount $1 /mnt/pico
-sudo cp build/z180_bus_spy.uf2 /mnt/pico
-sync
-sudo umount /mnt/pico
