8b2bad92de62f50b8e4c07fef8145417a962ffc2
Colin Maykish <crmaykish@gmail.com>
2022-10-27 22:48:08 -0400

    Add LCD test to hello program

--- firmware/hello.c
+++ firmware/hello.c
@@ -1,6 +1,7 @@
 #include <z180.h>
 #include "flounder.h"
 #include "asci.h"
+#include "lcd.h"
 
 int main()
 {
@@ -8,11 +9,16 @@ int main()
     int b = 0;
 
     uart_print("Blinking some LEDs...\r\n");
+    lcd_clear();
 
     while (1)
     {
         uart_print_dec(b);
         uart_print("\r\n");
+
+        lcd_print_hex(b);
+        lcd_print("\r\n");
+
         b++;
 
         z180_outp(0x4001, a);
